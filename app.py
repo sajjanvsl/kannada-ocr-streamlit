e12c1f03edaede09869ac64108db5c97c9373d3d
S P Sajjan <60458865+sajjanvsl@users.noreply.github.com>
2025-04-19 23:30:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader("📤 Upload Old Kannada Manuscript Image", typ
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with st.spinner("🔍 Running OCR..."):
         try:
