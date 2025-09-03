3f21970fdc3981c7f6612bd90b8a7eafa18d9caa
S P Sajjan <60458865+sajjanvsl@users.noreply.github.com>
2025-09-03 23:53:06 +0530

    Update app21.py

--- app21.py
+++ app21.py
@@ -15,10 +15,12 @@ import random
 import numpy as np
 import cv2
 import zipfile
+import gdown
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 os.environ['TESSDATA_PREFIX'] = r'C:\\Program Files\\Tesseract-OCR\\tessdata'
